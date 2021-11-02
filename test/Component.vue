902d0f45f27895e31f6a2774430062a768493fe8
Eduard Aksamitov <e@edaks.co>
2021-11-03 01:11:15 +0300

    build(deps): bump `stylelint`

---
+++ test/Component.vue
@@ -0,0 +1,35 @@
+<style lang="stylus">
+.test-block
+	--color: #fff
+
+	position: absolute
+	top: 0
+	right: 0
+	bottom: 0
+	left: 0
+	z-index: 10
+
+	display: block
+	float: right
+
+	width: 100px
+	height: 100px
+	padding: 10px
+	margin: 10px
+
+	font: normal 16px Helvetica, sans-serif
+	line-height: 1.3
+	color: #888
+	text-align: center
+
+	user-select: none
+
+	background-color: #eee
+
+	border: 1px solid #888
+	border-radius: 4px
+
+	opacity: 1
+
+	transition: all 1s
+</style>
